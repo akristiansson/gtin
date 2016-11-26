09e7f12b83b89ed49e728284cc3c2bdf81f4c7e7
17843 <arvid.christiansson@gmail.com>
2016-11-26 17:10:08 +0000

    Update is_gtin.js
    One liner...

--- is_gtin.js
+++ is_gtin.js
@@ -2,6 +2,6 @@ function isGtin(str, type = [8, 12, 13, 14, 18]) {
     return [].concat(type).includes(str.length) &&
         Uint8Array.from(str, x => parseInt(x, 10))
             .reduceRight(
-                (xc, x, i) => xc + (x << ((i & 1) + 1)) - x
+                (acc, x, i) => acc + (x << ((i & 1) + 1)) - x
             ) % 10 == 0;
 }
