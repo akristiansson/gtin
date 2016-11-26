d0539f431a60d553e159d183f2a70fe2732f42b4
17843 <arvid.christiansson@gmail.com>
2016-11-26 15:35:33 +0000

    Update is_gtin.js

--- is_gtin.js
+++ is_gtin.js
@@ -1,9 +1,8 @@
-let is_gtin = function (str) {
+function is_gtin(str) {
     try {
-        let digits = [...str].map(x => parseInt(x));
-        const checkdigit = digits.pop();
-        const m = digits.reduceRight((acc, x, i) => acc + (x << ((i & 1) + 1)) - x, 0) % 10;
-        return checkdigit == (m == 0 ? 0 : 10 - m);
+        const digits = [...str].map(x => parseInt(x, 10));
+        return [8, 12, 13, 14, 18].includes(digits.length) &&
+            digits.reduceRight((xc, x, i) => xc + (x << ((i & 1) + 1)) - x) % 10 == 0;
     }
     catch (e) {
         return false;
