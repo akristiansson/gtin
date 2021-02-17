5bbdac4cf8d83e289705766f8c4954e5d5e8a320
Arvid Kristiansson <arvid.christiansson@gmail.com>
2021-02-17 23:25:43 +0000

    Update isGtin.fs

--- isGtin.fs
+++ isGtin.fs
@@ -1,20 +1,8 @@
-let isGtin (x: String): Boolean = 
-
-    let checksum (digits: list<int>) = 
-        let accumulate xs acc multiplier recfun = 
-            match xs with
-            | [] -> acc
-            | x :: xs -> recfun xs (acc + multiplier * x)
-
-        let rec every xs acc = accumulate xs acc 3 other
-        and other xs acc = accumulate xs acc 1 every
-
-        every digits.Tail digits.Head
-
-    let ds = 
+let isGtin (x: string): bool = 
+    let checksum  = 
         if String.forall Char.IsDigit x then x.ToCharArray() else raise (System.ArgumentException("Non-digit found in string."))
         |> Array.map (fun x -> int x - int '0')
         |> Array.rev
-        |> List.ofArray 
-
-    checksum ds % 10 = 0
+        |> Array.mapi (fun x i -> if i % 2 = 0 then x else 3 * x)
+        |> Array.sum
+    checksum % 10 = 0
