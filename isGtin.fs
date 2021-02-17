2f428efaecc7b3a11ae055095650933af00f5431
Arvid Kristiansson <arvid.christiansson@gmail.com>
2021-02-17 23:49:29 +0000

    Update isGtin.fs

--- isGtin.fs
+++ isGtin.fs
@@ -3,6 +3,6 @@ let isGtin (x: string): bool =
         if String.forall Char.IsDigit x then x.ToCharArray() else raise (System.ArgumentException("Non-digit found in string."))
         |> Array.map (fun x -> int x - int '0')
         |> Array.rev
-        |> Array.mapi (fun x i -> if i % 2 = 0 then x else 3 * x)
+        |> Array.mapi (fun i x -> if i % 2 = 0 then x else 3 * x)
         |> Array.sum
     checksum % 10 = 0
